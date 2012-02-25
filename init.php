3a9a18125fa9954a8e6996d0e5c08d76e99ef526
zeebinz <zeebinz@gmail.com>
2012-02-25 21:20:05 +0000

    Checks whether UniversalClassLoader is already defined

--- init.php
+++ init.php
@@ -1,10 +1,13 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-// Register the Symfony autoloader
-include __DIR__.'/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+// Require the Symfony UniversalClassLoader
+if ( ! class_exists('Symfony\Component\ClassLoader\UniversalClassLoader', FALSE)) {
+	require __DIR__.'/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+}
 
+// Register the autoloader
 $loader = new Symfony\Component\ClassLoader\UniversalClassLoader();
 $loader->register();
 
-// Register the Symfony namespace
+// Register the Symfony namespace with the autoloader
 $loader->registerNamespace('Symfony', realpath(__DIR__.'/vendor'));
