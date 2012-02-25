a403081ed961bb248bc7e4b87254df630948f684
zeebinz <zeebinz@gmail.com>
2012-02-25 21:24:55 +0000

    Fix for coding standards

--- init.php
+++ init.php
@@ -1,7 +1,8 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-// Require the Symfony UniversalClassLoader
-if ( ! class_exists('Symfony\Component\ClassLoader\UniversalClassLoader', FALSE)) {
+if ( ! class_exists('Symfony\Component\ClassLoader\UniversalClassLoader', FALSE)) 
+{
+	// Require the Symfony UniversalClassLoader
 	require __DIR__.'/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 }
 
